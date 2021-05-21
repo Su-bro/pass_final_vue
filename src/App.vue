b95645d7a177a7db69be69eed07b02e7e35026df
osh <tngud941208@naver.com>
2021-05-21 13:20:30 +0900

    master

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style>
+@import 'https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i';
+@import 'https://fonts.googleapis.com/css?family=Raleway:300,400,700';
+@import 'https://fonts.googleapis.com/css?family=Pacifico';
+@import 'https://fonts.googleapis.com/css?family=PT+Serif';
+@import 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css';
+@import './resources/css/style.css';
+@import './resources/css/typography-default.css';
+@import './resources/css/skins/light_blue.css';
+@import './resources/css/custom.css';
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <div
+      class="banner dark-translucent-bg"
+      style="
+        background-image: url('https://img1.daumcdn.net/thumb/R720x0.q80/?scode=mtistory2&fname=http%3A%2F%2Fcfile6.uf.tistory.com%2Fimage%2F1815C13650113D2129E77B');
+        background-position: 50% 50%;
+      "
+    >
+      <!-- breadcrumb end  -->
+      <div class="container">
+        <div class="row justify-content-lg-center">
+          <div class="col-lg-8 text-center pv-20">
+            <h2 class="title">
+              <strong>문 화 조 아 </strong>
+            </h2>
+            <div class="separator"></div>
+            <p class="text-center" data-effect-delay="100">Moonhwa Joa</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- banner end -->
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
